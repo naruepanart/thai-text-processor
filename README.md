002c7f22b1b7dc8a6a5b0d52e1efeacccdaec29f
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-12-14 00:20:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Thai Text Processor
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE)
+
+## Overview
+
+The Thai Text Processor is a command-line tool for processing Thai text files. It utilizes a customizable word replacement mechanism to update and modify the content of text files.
+
+## Features
+
+- Thai language text processing
+- Customizable word replacement using a blacklist
+- Command-line interface for easy integration into scripts or workflows
+
+## Installation
+
+Clone the repository to your local machine and navigate to the project directory.
+
+```bash
+git clone https://github.com/naruepanart/thai-text-processor.git
+cd thai-text-processor
+```
+
+## Usage
+
+1. Ensure that you have Thai text files (with a `.txt` extension) in the same directory as the script.
+2. Run the script using the following command:
+
+```bash
+go run main.go
+```
+
+The script will process each Thai text file in the directory, applying word replacements based on the specified blacklist.
+
+## Customization
+
+### Adding or Modifying Word Replacements
+
+To customize word replacements, open the `initializeThaiBlacklist` function in the `main.go` file. Add or modify entries as needed. The format is `"original word": "replacement"`.
+
+Example:
+
+```go
+"example": "custom-replacement",
+```
+
+## Contributing
+
+If you would like to contribute to the project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them with descriptive commit messages.
+4. Push your changes to your fork.
+5. Open a pull request, explaining the purpose and benefits of your changes.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+Special thanks to the contributors and libraries that make this project possible.
+
+- [Regular Expressions Package](https://golang.org/pkg/regexp/)
+
+## Contact
+
+For any questions or concerns, feel free to reach out:
+
+- Email: naruepanart1201@gmail.com
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+// TextProcessor interface defines the method for processing text.
+type TextProcessor interface {
+	ProcessText(text string) string
+}
+
+// ThaiTextProcessor implements the TextProcessor interface for Thai text processing.
+type ThaiTextProcessor struct {
+	WordReplacements map[string]string
+}
+
+// ProcessText replaces words in the text based on the provided replacements.
+func (p ThaiTextProcessor) ProcessText(text string) string {
+	for word, replacement := range p.WordReplacements {
+		text = regexp.MustCompile(word).ReplaceAllString(text, replacement)
+	}
+	return text
+}
+
+// FileReader interface defines the method for reading a file.
+type FileReader interface {
+	ReadFile(filename string) ([]byte, error)
+}
+
+// OSFileReader implements the FileReader interface using the OS package.
+type OSFileReader struct{}
+
+// ReadFile reads the content of the file using the OS package.
+func (r OSFileReader) ReadFile(filename string) ([]byte, error) {
+	return os.ReadFile(filename)
+}
+
+// FileWriter interface defines the method for writing to a file.
+type FileWriter interface {
+	WriteFile(filename string, data []byte) error
+}
+
+// OSFileWriter implements the FileWriter interface using the OS package.
+type OSFileWriter struct{}
+
+// WriteFile writes the data to the file using the OS package.
+func (w OSFileWriter) WriteFile(filename string, data []byte) error {
+	return os.WriteFile(filename, data, 0644)
+}
+
+// FileManager manages the processing and updating of text files.
+type FileManager struct {
+	TextProcessor TextProcessor
+	FileReader    FileReader
+	FileWriter    FileWriter
+}
+
+// ProcessFile reads, processes, and writes a file using FileManager.
+func (m FileManager) ProcessFile(filename string) error {
+	data, err := m.FileReader.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("error reading file %s: %v", filename, err)
+	}
+
+	updatedText := m.TextProcessor.ProcessText(string(data))
+
+	err = m.FileWriter.WriteFile(filename, []byte(updatedText))
+	if err != nil {
+		return fmt.Errorf("error writing file %s: %v", filename, err)
+	}
+
+	fmt.Printf("Text in %s has been updated.\n", filename)
+
+	return nil
+}
+
+// getCurrentDirectory returns the current working directory.
+func getCurrentDirectory() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return dir
+}
+
+// initializeThaiBlacklist initializes a map of Thai words and their replacements.
+func initializeThaiBlacklist() map[string]string {
+	return map[string]string{
+		" ๆ":                     "ๆ",
+		"เรียกว่า":               " เรียกว่า ",
+		"และ":                    " และ",
+		"ซึ่ง":                   " ซึ่ง",
+		"สามารถ":                 " สามารถ",
+		"ฟังก์ชัน":               "ฟังก์ชั่น",
+		"ลูชัน":                  "ลูชั่น",
+		"ต่อๆ ไป":                "ต่อๆไป",
+		"อัลกอริทึม":             "อัลกอริทึ่ม",
+		"อัลกอริธึม":             "อัลกอริธึ่ม",
+		"กาแลคซี":                "กาแล็คซี่",
+		"กาแล็กซี":               "กาแล็กซี่",
+		"แอนโดรเมดา":             "แอนโดรเมด้า",
+		"สสาร":                   "สะสาร",
+		"ไดนามิก":                "ไดนามิค",
+		"เอนโทรปี":               "เอนโทรปี้",
+		"ยูโรปา":                 "ยูโรป้า",
+		"ฮับเบิล":                "ฮับเบิ้ล",
+		"ควอนตัม":                "ควอนตั้ม",
+		"ละลาย":                 "ละลาย ",
+		"พลาสมา":                 "พลาสม่า",
+		"มีนัย":                  "มีนัยะ",
+		"ยักษ์":                  "ยัก",
+		"แอริโซนา":               "แอริโซน่า",
+		"ลิเธียม":                "ลิเธี่ยม",
+		" กม. ":                  " กิโลเมตร ",
+		"อาร์เทมิส":              "อาร์ทิมิส",
+		" CO2 ":                  " C O 2 ",
+		" EVs ":                  " E V ",
+		"GPS":                    "G P S",
+		"Square Kilometer Array": "สแควร์ กิโลมิเตอร์ อาร์เรย์",
+		"SpaceX":                 "สเปซเอ็กซ์",
+		"Blue Origin":            "บลูออริจิ้น",
+		"Curiosity":              "คิวริออซิตี้",
+		"Galileo":                "กาลิเลโอ",
+		"STEM":                   "สะเต็ม",
+		"NASA":                   "นาซ่า",
+		"Perseverance":           "เพอซะเวียแร้น",
+		"°F":                     "องศาฟาเรนไฮต์",
+		"°C":                     "องศาเซลเซียส",
+		":":                      "",
+		"โดยรวมแล้ว ":            "",
+		"โดยสรุป":                "",
+	}
+}
+
+func main() {
+	scriptDir := getCurrentDirectory()
+
+	txtFiles, err := filepath.Glob(filepath.Join(scriptDir, "*.txt"))
+	if err != nil {
+		panic(err)
+	}
+
+	blacklist := initializeThaiBlacklist()
+
+	fileManager := FileManager{
+		TextProcessor: ThaiTextProcessor{WordReplacements: blacklist},
+		FileReader:    OSFileReader{},
+		FileWriter:    OSFileWriter{},
+	}
+
+	for _, fileName := range txtFiles {
+		if err := fileManager.ProcessFile(fileName); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
