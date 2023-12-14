74b72e53b88c960cabdfad9a6ee9a4223defc48e
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2023-12-15 03:02:29 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,32 +9,32 @@ import (
 )
 
 // TextProc represents a text processing utility.
-type TextProc struct{ R map[string]string }
+type TextProc struct{ Replacements map[string]string }
 
 // Process replaces words in the given text based on predefined replacements.
-func (p TextProc) Process(t string) string {
-	for w, r := range p.R {
-		t = regexp.MustCompile(w).ReplaceAllString(t, r)
+func (p TextProc) Process(text string) string {
+	for word, replacement := range p.Replacements {
+		text = regexp.MustCompile(word).ReplaceAllString(text, replacement)
 	}
-	return t
+	return text
 }
 
 // FileMgr manages file-related operations using a TextProc.
-type FileMgr struct{ P TextProc }
+type FileMgr struct{ Processor TextProc }
 
 // ProcessFile reads, processes, and writes the updated text to a file.
-func (m FileMgr) ProcessFile(fname string) error {
-	data, err := os.ReadFile(fname)
+func (m FileMgr) ProcessFile(filename string) error {
+	data, err := os.ReadFile(filename)
 	if err != nil {
-		return fmt.Errorf("error reading file %s: %v", fname, err)
+		return fmt.Errorf("error reading file %s: %v", filename, err)
 	}
 
-	ut := m.P.Process(string(data))
-	if err := os.WriteFile(fname, []byte(ut), 0644); err != nil {
-		return fmt.Errorf("error writing file %s: %v", fname, err)
+	updatedText := m.Processor.Process(string(data))
+	if err := os.WriteFile(filename, []byte(updatedText), 0644); err != nil {
+		return fmt.Errorf("error writing file %s: %v", filename, err)
 	}
 
-	fmt.Printf("Text in %s has been updated.\n", fname)
+	fmt.Printf("Text in %s has been updated.\n", filename)
 	return nil
 }
 
@@ -46,23 +46,23 @@ func InitProc() TextProc {
 	}
 	defer file.Close()
 
-	var r map[string]string
-	if err := json.NewDecoder(file).Decode(&r); err != nil {
+	var replacements map[string]string
+	if err := json.NewDecoder(file).Decode(&replacements); err != nil {
 		panic(fmt.Errorf("error decoding blacklist.json: %v", err))
 	}
 
-	return TextProc{R: r}
+	return TextProc{Replacements: replacements}
 }
 
 func main() {
-	txtFiles, err := filepath.Glob("*.txt")
+	textFiles, err := filepath.Glob("*.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	manager := FileMgr{P: InitProc()}
-	for _, fname := range txtFiles {
-		if err := manager.ProcessFile(fname); err != nil {
+	manager := FileMgr{Processor: InitProc()}
+	for _, filename := range textFiles {
+		if err := manager.ProcessFile(filename); err != nil {
 			fmt.Println(err)
 		}
 	}
