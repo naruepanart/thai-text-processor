947c39b9584c01d2d7045c797475e64f6e17c3bf
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-01-09 20:24:10 +0700

    Update main.go

--- blacklist-novel/main.go
+++ blacklist-novel/main.go
@@ -8,69 +8,50 @@ import (
 	"regexp"
 )
 
-type Replacements map[string]string
-
-func processFile(filename string, repl Replacements) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("open file %s: %v", filename, err)
-	}
-	defer file.Close()
-
-	stat, err := file.Stat()
-	if err != nil {
-		return fmt.Errorf("stat file %s: %v", filename, err)
-	}
-
-	data := make([]byte, stat.Size())
-	_, err = file.Read(data)
-	if err != nil {
-		return fmt.Errorf("read file %s: %v", filename, err)
-	}
-
-	updatedText := processText(string(data), repl)
-	if err := os.WriteFile(filename, []byte(updatedText), 0644); err != nil {
-		return fmt.Errorf("write file %s: %v", filename, err)
-	}
-
-	fmt.Printf("Text in %s updated.\n", filename)
-	return nil
-}
-
-func processText(text string, repl Replacements) string {
-	for pattern, replacement := range repl {
-		text = regexp.MustCompile(pattern).ReplaceAllString(text, replacement)
-	}
-	return text
-}
-
-func loadReplacements(filename string) Replacements {
-	file, err := os.Open(filename)
-	if err != nil {
-		panic(fmt.Errorf("open %s: %v", filename, err))
-	}
-	defer file.Close()
-
-	var repl Replacements
-	if err := json.NewDecoder(file).Decode(&repl); err != nil {
-		panic(fmt.Errorf("decode %s: %v", filename, err))
-	}
-
-	return repl
-}
-
 func main() {
+	// Load replacements from blacklist-nv.json
+	blacklistFile, err := os.Open("blacklist-nv.json")
+	if err != nil {
+		panic(fmt.Errorf("failed to open blacklist-nv.json: %v", err))
+	}
+	defer blacklistFile.Close()
+
+	// Parse JSON content from blacklist file
+	var replacements map[string]string
+	if err := json.NewDecoder(blacklistFile).Decode(&replacements); err != nil {
+		panic(fmt.Errorf("failed to decode blacklist-nv.json: %v", err))
+	}
+
+	// Add custom replacement for number ranges
+	replacements[`(\d+)-(\d+)`] = `${1}ถึง${2}`
+
+	// Process all text files in the current directory
 	txtFiles, err := filepath.Glob("*.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	repl := loadReplacements("blacklist-nv.json")
-	repl[`(\d+)-(\d+)`] = `${1}ถึง${2}`
-
 	for _, filename := range txtFiles {
-		if err := processFile(filename, repl); err != nil {
-			fmt.Println(err)
+		// Read file content into memory
+		fileContent, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println(fmt.Errorf("failed to read file %s: %v", filename, err))
+			continue
 		}
+
+		// Process text and update file content
+		updatedText := string(fileContent)
+		for pattern, replacement := range replacements {
+			updatedText = regexp.MustCompile(pattern).ReplaceAllString(updatedText, replacement)
+		}
+
+		// Use stack memory to write updated content to file
+		err = os.WriteFile(filename, []byte(updatedText), os.ModePerm)
+		if err != nil {
+			fmt.Println(fmt.Errorf("failed to write file %s: %v", filename, err))
+			continue
+		}
+
+		fmt.Printf("Text in %s updated.\n", filename)
 	}
 }
