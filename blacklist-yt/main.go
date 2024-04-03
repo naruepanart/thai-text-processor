8f1a910b7c543c1d21ed0bd55999ed5253abfb42
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-04-04 04:53:35 +0700

    Update main.go

--- blacklist-yt/main.go
+++ blacklist-yt/main.go
@@ -10,37 +10,46 @@ import (
 )
 
 func main() {
-	b, err := os.Open("blacklist.json")
+	// Open and parse the blacklist JSON file
+	f, err := os.Open("blacklist.json")
 	if err != nil {
 		panic(fmt.Errorf("open blacklist.json: %v", err))
 	}
-	defer b.Close()
+	defer f.Close()
 
-	r := make(map[string]string)
-	if err := json.NewDecoder(b).Decode(&r); err != nil {
+	blacklist := make(map[string]string)
+	if err := json.NewDecoder(f).Decode(&blacklist); err != nil {
 		panic(fmt.Errorf("decode blacklist.json: %v", err))
 	}
 
-	p := make([]*regexp.Regexp, 0, len(r))
-	v := make([]string, 0, len(r))
-	for pattern, value := range r {
-		p = append(p, regexp.MustCompile(pattern))
-		v = append(v, value)
-	}
-
-	t, err := filepath.Glob("*.txt")
-	if err != nil {
-		panic(err)
-	}
-
-	for _, f := range t {
-		file, err := os.Open(f)
+	// Compile regular expressions from the blacklist
+	regexps := make([]*regexp.Regexp, 0, len(blacklist))
+	replacements := make([][]byte, 0, len(blacklist))
+	for pattern, replacement := range blacklist {
+		re, err := regexp.Compile(pattern)
 		if err != nil {
-			fmt.Println(fmt.Errorf("open file %s: %v", f, err))
+			panic(fmt.Errorf("compile regex %s: %v", pattern, err))
+		}
+		regexps = append(regexps, re)
+		replacements = append(replacements, []byte(replacement))
+	}
+
+	// Find all text files in the current directory
+	files, err := filepath.Glob("*.txt")
+	if err != nil {
+		panic(fmt.Errorf("glob: %v", err))
+	}
+
+	// Process each text file
+	for _, filename := range files {
+		file, err := os.OpenFile(filename, os.O_RDWR, os.ModePerm)
+		if err != nil {
+			fmt.Println(fmt.Errorf("open file %s: %v", filename, err))
 			continue
 		}
 		defer file.Close()
 
+		// Process file content
 		var updated []byte
 		buf := make([]byte, 1024)
 
@@ -50,23 +59,29 @@ func main() {
 				break
 			}
 			if err != nil {
-				fmt.Println(fmt.Errorf("read file %s: %v", f, err))
+				fmt.Println(fmt.Errorf("read file %s: %v", filename, err))
 				break
 			}
 
+			// Apply regular expression replacements
 			text := buf[:n]
-			for i, pattern := range p {
-				text = pattern.ReplaceAll(text, []byte(v[i]))
+			for i, re := range regexps {
+				text = re.ReplaceAll(text, replacements[i])
 			}
 
 			updated = append(updated, text...)
 		}
 
-		if err = os.WriteFile(f, updated, os.ModePerm); err != nil {
-			fmt.Println(fmt.Errorf("write file %s: %v", f, err))
+		// Write updated content back to the file
+		if err := os.Truncate(filename, 0); err != nil {
+			fmt.Println(fmt.Errorf("truncate file %s: %v", filename, err))
+			continue
+		}
+		if _, err := file.WriteAt(updated, 0); err != nil {
+			fmt.Println(fmt.Errorf("write file %s: %v", filename, err))
 			continue
 		}
 
-		fmt.Printf("Text in %s updated.\n", f)
+		fmt.Printf("Text in %s updated.\n", filename)
 	}
 }
