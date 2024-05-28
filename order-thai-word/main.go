59a556066f1b4b249b6c817ae371b84648babc04
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-05-28 18:11:08 +0700

    Revert "Update main.go"
    This reverts commit a3686a6923cc59319280d743b6c2540c4b446e32.

--- order-thai-word/main.go
+++ order-thai-word/main.go
@@ -7,55 +7,19 @@ import (
 )
 
 func main() {
-	// Read the blacklist JSON file into a map of strings to interfaces
-	d, err := readJSONFile("./blacklist.json")
+	f, err := os.Open("./blacklist.json")
 	if err != nil {
 		fmt.Println(err)
 		return
-	}
-
-	// Remove any duplicate entries from the map
-	d = removeDuplicates(d)
-
-	// Write the updated blacklist JSON file
-	err = writeJSONFile("./blacklist.json", d)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-func readJSONFile(path string) (map[string]interface{}, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
 	}
 	defer f.Close()
 
 	var d map[string]interface{}
 	if err := json.NewDecoder(f).Decode(&d); err != nil {
-		return nil, err
+		fmt.Println(err)
+		return
 	}
 
-	return d, nil
-}
-
-func writeJSONFile(path string, d map[string]interface{}) error {
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	err = json.NewEncoder(f).Encode(d)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func removeDuplicates(d map[string]interface{}) map[string]interface{} {
 	m := make(map[string]bool)
 	for k := range d {
 		if m[k] {
@@ -64,5 +28,18 @@ func removeDuplicates(d map[string]interface{}) map[string]interface{} {
 			m[k] = true
 		}
 	}
-	return d
+
+	f, err = os.Create("./blacklist.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	if err := json.NewEncoder(f).Encode(d); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Duplicates removed successfully.")
 }
