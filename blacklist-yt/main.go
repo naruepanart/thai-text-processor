4efee419226709d535594b48fe163bf838dea86b
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-05-28 09:55:50 +0700

    Update main.go

--- blacklist-yt/main.go
+++ blacklist-yt/main.go
@@ -10,97 +10,70 @@ import (
 )
 
 func main() {
-	err := processFilesWithBlacklist()
+	// Load the blacklist
+	blacklistFile, err := os.Open("blacklist.json")
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("Error loading blacklist: %v\n", err)
+		return
 	}
-}
-
-func processFilesWithBlacklist() error {
-	blacklist, err := loadBlacklist("blacklist.json")
-	if err != nil {
-		return fmt.Errorf("loading blacklist: %v", err)
-	}
-
-	regexPatterns, replacements, err := compileBlacklist(blacklist)
-	if err != nil {
-		return fmt.Errorf("compiling regex: %v", err)
-	}
-
-	textFiles, err := filepath.Glob("*.txt")
-	if err != nil {
-		return fmt.Errorf("glob: %v", err)
-	}
-
-	for _, filename := range textFiles {
-		if err := processFileWithBlacklist(filename, regexPatterns, replacements); err != nil {
-			fmt.Printf("Error processing %s: %v\n", filename, err)
-		}
-	}
-
-	return nil
-}
-
-func loadBlacklist(filename string) (map[string]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
+	defer blacklistFile.Close()
 
 	var blacklist map[string]string
-	if err := json.NewDecoder(file).Decode(&blacklist); err != nil {
-		return nil, err
+	if err := json.NewDecoder(blacklistFile).Decode(&blacklist); err != nil {
+		fmt.Printf("Error decoding blacklist: %v\n", err)
+		return
 	}
-	return blacklist, nil
-}
 
-func compileBlacklist(blacklist map[string]string) ([]*regexp.Regexp, [][]byte, error) {
+	// Compile the blacklist regex patterns and replacements
 	var regexPatterns []*regexp.Regexp
 	var replacements [][]byte
 
 	for pattern, replacement := range blacklist {
 		re, err := regexp.Compile(pattern)
 		if err != nil {
-			return nil, nil, err
+			fmt.Printf("Error compiling regex: %v\n", err)
+			return
 		}
 		regexPatterns = append(regexPatterns, re)
 		replacements = append(replacements, []byte(replacement))
 	}
 
-	return regexPatterns, replacements, nil
-}
-
-func processFileWithBlacklist(filename string, regexPatterns []*regexp.Regexp, replacements [][]byte) error {
-	content, err := os.ReadFile(filename)
+	// Find all text files
+	textFiles, err := filepath.Glob("*.txt")
 	if err != nil {
-		return err
+		fmt.Printf("Error finding text files: %v\n", err)
+		return
 	}
 
-	updatedContent := content
-	for i, pattern := range regexPatterns {
-		updatedContent = pattern.ReplaceAll(updatedContent, replacements[i])
-	}
-
-	// Convert B.E. years to A.D. if they start with "25"
-	updatedContent = convertBEToAD(updatedContent)
-
-	if err := os.WriteFile(filename, updatedContent, os.ModePerm); err != nil {
-		return err
-	}
-
-	fmt.Printf("Text in %s updated.\n", filename)
-	return nil
-}
-
-func convertBEToAD(content []byte) []byte {
-	re := regexp.MustCompile(`(\d{4})`)
-	return re.ReplaceAllFunc(content, func(match []byte) []byte {
-		yearBE, _ := strconv.Atoi(string(match))
-		if yearBE >= 2400 {
-			yearAD := yearBE - 543
-			return []byte(strconv.Itoa(yearAD))
+	// Process each text file
+	for _, filename := range textFiles {
+		content, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Printf("Error reading file %s: %v\n", filename, err)
+			continue
 		}
-		return match
-	})
+
+		updatedContent := content
+		for i, pattern := range regexPatterns {
+			updatedContent = pattern.ReplaceAll(updatedContent, replacements[i])
+		}
+
+		// Convert B.E. years to A.D. if they start with "25"
+		re := regexp.MustCompile(`(\d{4})`)
+		updatedContent = re.ReplaceAllFunc(updatedContent, func(match []byte) []byte {
+			yearBE, _ := strconv.Atoi(string(match))
+			if yearBE >= 2400 {
+				yearAD := yearBE - 543
+				return []byte(strconv.Itoa(yearAD))
+			}
+			return match
+		})
+
+		if err := os.WriteFile(filename, updatedContent, os.ModePerm); err != nil {
+			fmt.Printf("Error writing to file %s: %v\n", filename, err)
+			continue
+		}
+
+		fmt.Printf("Text in %s updated.\n", filename)
+	}
 }
